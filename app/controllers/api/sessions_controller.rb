fa04d43826fecfcf5c4e43e615f1abb7bc0b7583
Justin Kilburn <ThatMoonMan@gmail.com>
2022-12-03 15:08:50 -0500

    user auth backend

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,32 @@
+class Api::SessionsController < ApplicationController
+
+  def show
+    banana
+    if current_user
+      @user = current_user
+      render 'api/users/show'
+    else
+      render json: { user: nil }
+    end
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      credential: params[:credential],
+      password: params[:password]
+    )
+    if @user
+      login!(@user)
+      render 'api/users/show'
+    else
+      render json: { errors: ['The provided credentials were invalid.'] }, status: :unauthorized
+    end
+  end
+
+  def destroy
+    return unless current_user
+    
+    logout! 
+    render json: { message: 'success' }
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,74 @@
 class ApplicationController < ActionController::API
     
-    before_action :snake_case_params
+  rescue_from StandardError, with: :unhandled_error
+  rescue_from ActionController::InvalidAuthenticityToken,
+      with: :invalid_authenticity_token
 
-    private
+  include ActionController::RequestForgeryProtection
+  protect_from_forgery with: :exception
+
+  before_action :snake_case_params, :attach_authenticity_token
+
+  def current_user
+      @current_user ||= User.find_by(session_token: session[:session_token])
+  end
     
-    def snake_case_params
+  def login!(user)
+      session[:session_token] = user.reset_session_token!
+      @current_user = user
+  end
+    
+  def logout!
+      current_user.reset_session_token!
+      session[:session_token] = nil
+      @current_user = nil
+  end
+    
+  def require_logged_in
+      unless current_user
+          render json: { message: 'Unauthorized' }, status: :unauthorized 
+      end
+  end
+
+  def test
+      if params.has_key?(:login)
+          login!(User.first)
+      elsif params.has_key?(:logout)
+          logout!
+      end
+      
+      if current_user
+          render json: { user: current_user.slice('id', 'username', 'session_token') }
+      else
+          render json: ['No current user']
+      end
+  end
+
+  private
+
+  def attach_authenticity_token
+      headers['X-CSRF-Token'] = masked_authenticity_token(session)
+  end
+  
+  def snake_case_params
       params.deep_transform_keys!(&:underscore)
-    end
+  end
+
+  def invalid_authenticity_token
+      render json: { message: 'Invalid authenticity token' }, 
+          status: :unprocessable_entity
+  end
+    
+  def unhandled_error(error)
+      if request.accepts.first.html?
+          raise error
+      else
+          @message = "#{error.class} - #{error.message}"
+          @stack = Rails::BacktraceCleaner.new.clean(error.backtrace)
+          render 'api/errors/internal_server_error', status: :internal_server_error
+          
+          logger.error "\n#{@message}:\n\t#{@stack.join("\n\t")}\n"
+      end
+  end
 
 end
