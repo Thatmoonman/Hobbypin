1831facea45ffa5834f98c8b639770b62b9e137e
Justin Kilburn <ThatMoonMan@gmail.com>
2022-12-03 15:50:27 -0500

    front end auth connect to backkend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,19 +30,19 @@ class ApplicationController < ActionController::API
       end
   end
 
-  def test
-      if params.has_key?(:login)
-          login!(User.first)
-      elsif params.has_key?(:logout)
-          logout!
-      end
+#   def test
+#       if params.has_key?(:login)
+#           login!(User.first)
+#       elsif params.has_key?(:logout)
+#           logout!
+#       end
       
-      if current_user
-          render json: { user: current_user.slice('id', 'username', 'session_token') }
-      else
-          render json: ['No current user']
-      end
-  end
+#       if current_user
+#           render json: { user: current_user.slice('id', 'username', 'session_token') }
+#       else
+#           render json: ['No current user']
+#       end
+#   end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  post 'api/test', to: 'application#test'
+  # post 'api/test', to: 'application#test'
 
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create]
