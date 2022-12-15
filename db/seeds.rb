89e76f0854183f4544f2913a82b8e6584f6f898d
Justin Kilburn <ThatMoonMan@gmail.com>
2022-12-15 09:25:47 -0500

    seed errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,7 @@ ApplicationRecord.transaction do
     User.destroy_all
     Pin.destroy_all
     Board.destroy_all
+    PinnedBoard.destroy_all
   
     puts "Resetting primary keys..."
     # For easy testing, so that after seeding, the first `User` has `id` of 1
