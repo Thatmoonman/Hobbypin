bccfc9d72336b1f47c20e03febeabd9fc28429d0
Justin Kilburn <ThatMoonMan@gmail.com>
2023-01-12 11:15:32 -0500

    pin doesnt exist

--- frontend/src/components/Pins/PinShowPage/index.js
+++ frontend/src/components/Pins/PinShowPage/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux"
-import { Redirect, useHistory, useParams } from "react-router-dom";
+import { Link, Redirect, useHistory, useParams } from "react-router-dom";
 import { fetchBoards, getBoards } from "../../../store/board";
 import { fetchPin, getPin } from "../../../store/pins";
 import { Modal } from "../../../context/Modal";
@@ -25,10 +25,12 @@ const PinShowPage = () => {
 
     const [selectBoard, setSelectBoard] = useState(boards && boards.length ? boards[0] : "")
     const [showSelectBoard, setShowSelectBoard] = useState(false)
+    const [loaded, setLoaded] = useState(false)
 
     useEffect(() => {
         dispatch(fetchPin(userId, pinId))
         if (currentUser) dispatch(fetchBoards(currentUser.id))
+        setLoaded(true)
     }, [userId, pinId, currentUser])
 
     const handleAddBoardClick = (e, board) => {
@@ -56,38 +58,66 @@ const PinShowPage = () => {
 
     if (!currentUser) return <Redirect to="/" />
 
-    return (
-        <div className={`pinShowContainer ${pinOrientation}`}>
-            <div className="imageContainer">
-                <img src={pin.photoUrl} alt=""/>
-            </div>
-            <div className="pinShowDetails">
-                <div className="boardSave">
-                    <div onClick={toggleSelectBoardModal}>
-                        {selectBoard.title}
-                        <i className="fa-solid fa-chevron-down" />
-                    </div>
-                    <button className="saveButton" onClick={handleSavePin}>
-                        Save
-                    </button>
+    const loadPin = () => {
+            
+        if (loaded) {
+            return (
+                <div className="notAvailable">
+                    <p>Pin does not exist.</p>
+                    <Link to="/" className="notAvailableLink">Return to Home</Link>
                 </div>
-                <h1>{pin.title}</h1>
-                <p>{pin.description}</p>
-                <CommentsIndex />
-                <CreateCommentForm currentUser={currentUser}/>
-            </div>
-            {showSelectBoard && 
-                <Modal onClose={() => setShowSelectBoard(false)} >
-                    <div className="selectBoardForm">
-                        {boards.map(board => (
-                            <div key={board.id} onClick={(e) => handleAddBoardClick(e, board)}>
-                                {board.title}
-                                <button className="saveButton" onClick={handleAddBoardButton}>Save</button>
-                            </div>
-                        ))}
+
+            )
+        } else {
+            return (
+                <div className="loadingUser">
+                    {/* <p>Loading Pin</p> */}
+                </div>
+            )
+        }   
+    }
+
+
+    return (
+        <>
+        {loaded && pin.id ? (
+            <div className={`pinShowContainer ${pinOrientation}`}>
+                <div className="imageContainer">
+                    <img src={pin.photoUrl} alt=""/>
+                </div>
+                <div className="pinShowDetails">
+                    <div className="boardSave">
+                        <div onClick={toggleSelectBoardModal}>
+                            {selectBoard.title}
+                            <i className="fa-solid fa-chevron-down" />
+                        </div>
+                        <button className="saveButton" onClick={handleSavePin}>
+                            Save
+                        </button>
                     </div>
-                </Modal>}
-        </div>
+                    <h1>{pin.title}</h1>
+                    <p>{pin.description}</p>
+                    <CommentsIndex />
+                    <CreateCommentForm currentUser={currentUser}/>
+                </div>
+                {showSelectBoard && 
+                    <Modal onClose={() => setShowSelectBoard(false)} >
+                        <div className="selectBoardForm">
+                            {boards.map(board => (
+                                <div key={board.id} onClick={(e) => handleAddBoardClick(e, board)}>
+                                    {board.title}
+                                    <button className="saveButton" onClick={handleAddBoardButton}>Save</button>
+                                </div>
+                            ))}
+                        </div>
+                    </Modal>}
+            </div>
+        ) : (
+            <>
+                {loadPin()}
+            </>
+        )}
+        </>
     )
 }
 
