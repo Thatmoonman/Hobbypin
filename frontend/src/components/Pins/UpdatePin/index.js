84489bed9ca3ce463be563e7394671ab745a33f7
Justin Kilburn <ThatMoonMan@gmail.com>
2023-01-23 14:53:22 -0500

    update pin

--- frontend/src/components/Pins/UpdatePin/index.js
+++ frontend/src/components/Pins/UpdatePin/index.js
@@ -34,6 +34,28 @@ const UpdatePin = (props) => {
                     onChange={(e) => setTitle(e.target.value)}
                 />
             </div>
+            <div className="focusInputs">
+                {title.length <= 25 && title.length >= 3 ? (
+                    <>
+                        <div>Title must be between 3 and 25 characters</div>
+                        <div>{title.length}/{25 - title.length}</div>
+                    </>
+                ) : (
+                    <>
+                    {title.length < 3 ? (
+                        <>
+                            <div className="focusInputsError">Title must be between 3 and 25 characters</div>
+                            <div className="focusInputsError">{title.length}/{25 - title.length}</div>
+                        </>
+                    ) : (
+                        <>
+                            <div className="focusInputsError">Title too long</div>
+                            <div className="focusInputsError">{title.length}/25</div>
+                        </>
+                    )}
+                    </>
+                )}
+            </div>
             <div className="updatePinInputContainer">
                 <label htmlFor="description">Description: </label>
                 <textarea id="description"
@@ -41,6 +63,19 @@ const UpdatePin = (props) => {
                     onChange={(e) => setDescription(e.target.value)}
                 />
             </div>
+            <div className="focusInputs">
+                                    {description.length <= 100 ? (
+                                        <>
+                                            <div>A short description is best  </div>
+                                            <div>{description.length}/{100 - description.length}</div>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <div className="focusInputsError">Description too long</div>
+                                            <div className="focusInputsError">{description.length}/100</div>
+                                        </>
+                                    )}
+                                </div>
             <button className="saveButton">Save</button>
         </form>
     )
