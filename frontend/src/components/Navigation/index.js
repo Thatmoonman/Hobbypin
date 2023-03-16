8e867d7a5e59bcd3c020c43505958dbc1fe56edd
Justin Kilburn <ThatMoonMan@gmail.com>
2023-03-16 10:52:41 -0400

    mobelize logged out nav bar

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -13,6 +13,8 @@ import logo from './Hobbypinlogo.png'
 function Navigation(){
     const sessionUser = useSelector(state => state.session.user);
   
+    const isMobile = /Android|iPhone/i.test(navigator.userAgent)
+    console.log(isMobile)
 
     const [notificationIsHovering, setNotificationIsHovering] = useState(false)
     const [messageIsHovering, setMessageIsHovering] = useState(false)
@@ -39,78 +41,148 @@ function Navigation(){
         } 
     }
 
-    let sessionLinks;
-    if (sessionUser) {
-        const userId = sessionUser.id
-
-        sessionLinks = (
-        <div className="navBar">
-            <NavLink exact to="/" className="logoNavLink"><img src={logo} alt=""/></NavLink>
-            <NavLink exact to="/" className="homeNavLink"><p>Home</p></NavLink>
-            <div className='aboutNav' onClick={() => setShowAboutModal(true)}>About</div>
-            {showAboutModal && <AboutModal setShowAboutModal={setShowAboutModal} />}
-            <CreateDropdown />
-            <SearchBar />
-            <Link to={{pathname: "https://www.linkedin.com/in/justin-kilburn-3aa38a54/"}} 
-                target="_blank"
-                className='navIcon' 
-                onMouseEnter={() => handleMouseOver('notifications')} 
-                onMouseOut={() => handleMouseOut('notifications')}>
-                <i className="fa-brands fa-linkedin"
-                    onMouseEnter={() => handleMouseOver('notifications')} 
-                    onMouseOut={() => handleMouseOut('notifications')}>
-                </i>
-            </Link>
-                {notificationIsHovering && <div className='navTooltip navNotifications'>LinkedIn</div>}
-            <Link to={{pathname: "https://github.com/Thatmoonman"}}
-                target="_blank"
-                className='navIcon'
-                onMouseEnter={() => handleMouseOver('github')}
-                onMouseOut={() => handleMouseOut('github')}>
-                <i className="fa-brands fa-github"
-                    onMouseEnter={() => handleMouseOver('github')}
-                    onMouseOut={() => handleMouseOut('github')}>
-                </i>
-            </Link>
-                {messageIsHovering && <div className='navTooltip navGithub'>Github</div>}
-            <NavLink exact to={`/users/${userId}`} className='navIcon' 
-                onMouseEnter={() => handleMouseOver('profile')} 
-                onMouseOut={() => handleMouseOut('profile')}>
-                <div className='profileNavPicContainerOuter'>
-                    <div className='profileNavPicContainerInner'>
-                        <img src={sessionUser.profilePic} alt=""
+    const sessionLinks = () => {
+        if (isMobile) {
+            if (sessionUser) {
+                const userId = sessionUser.id
+                return (
+                    <div className="navBar">
+                        <NavLink exact to="/" className="logoNavLink"><img src={logo} alt=""/></NavLink>
+                        <NavLink exact to="/" className="homeNavLink"><p>Home</p></NavLink>
+                        <div className='aboutNav' onClick={() => setShowAboutModal(true)}>About</div>
+                        {showAboutModal && <AboutModal setShowAboutModal={setShowAboutModal} />}
+                        <CreateDropdown />
+                        <SearchBar />
+                        <Link to={{pathname: "https://www.linkedin.com/in/justin-kilburn-3aa38a54/"}} 
+                            target="_blank"
+                            className='navIcon' 
+                            onMouseEnter={() => handleMouseOver('notifications')} 
+                            onMouseOut={() => handleMouseOut('notifications')}>
+                            <i className="fa-brands fa-linkedin"
+                                onMouseEnter={() => handleMouseOver('notifications')} 
+                                onMouseOut={() => handleMouseOut('notifications')}>
+                            </i>
+                        </Link>
+                            {notificationIsHovering && <div className='navTooltip navNotifications'>LinkedIn</div>}
+                        <Link to={{pathname: "https://github.com/Thatmoonman"}}
+                            target="_blank"
+                            className='navIcon'
+                            onMouseEnter={() => handleMouseOver('github')}
+                            onMouseOut={() => handleMouseOut('github')}>
+                            <i className="fa-brands fa-github"
+                                onMouseEnter={() => handleMouseOver('github')}
+                                onMouseOut={() => handleMouseOut('github')}>
+                            </i>
+                        </Link>
+                            {messageIsHovering && <div className='navTooltip navGithub'>Github</div>}
+                        <NavLink exact to={`/users/${userId}`} className='navIcon' 
                             onMouseEnter={() => handleMouseOver('profile')} 
-                            onMouseOut={() => handleMouseOut('profile')}
-                        />
+                            onMouseOut={() => handleMouseOut('profile')}>
+                            <div className='profileNavPicContainerOuter'>
+                                <div className='profileNavPicContainerInner'>
+                                    <img src={sessionUser.profilePic} alt=""
+                                        onMouseEnter={() => handleMouseOver('profile')} 
+                                        onMouseOut={() => handleMouseOut('profile')}
+                                    />
+                                </div>
+                            </div>
+                        </NavLink>
+                            {profileIsHovering && <div className='navTooltip navProfile' >Your profile</div>}
+                        <ProfileButton user={sessionUser} />
                     </div>
-                </div>
-            </NavLink>
-                {profileIsHovering && <div className='navTooltip navProfile' >Your profile</div>}
-            <ProfileButton user={sessionUser} />
-        </div>
-        );
-    } else {
-        sessionLinks = (
-            <div className='navBar snap'>
-                <NavLink exact to="/" className="logoNavLink"><img src="./Hobbypinlogo.png" alt=""/><h1>Hobbypin</h1></NavLink>
-                <div className='rightNav'>
-                    <div className='externalLinks'>
-                        <div className='externalLink' onClick={() => setShowAboutModal(true)}>About</div>
-                        <Link to={{pathname: "https://github.com/Thatmoonman"}} target="_blank" className='externalLink'>Github</Link>
-                        <Link to={{pathname: "https://www.linkedin.com/in/justin-kilburn-3aa38a54/"}} target="_blank"className='externalLink'>LinkedIn</Link>
+                );
+            } else {
+                return (
+                    <div className='navBarMobile snap'>
+                        <div className='topNav'>
+                            <NavLink exact to="/" className="logoNavLink logoNavLinkMobile"><img src="./Hobbypinlogo.png" alt=""/></NavLink>
+                            <div className='rightNav'>
+                                <LoginFormModal />
+                                <SignupFormModal />
+                            </div>
+                        </div>
+                        <div className='externalLinks externalLinksMobile'>
+                            <div className='externalLink' onClick={() => setShowAboutModal(true)}>About</div>
+                            <Link to={{pathname: "https://github.com/Thatmoonman"}} target="_blank" className='externalLink'>Github</Link>
+                            <Link to={{pathname: "https://www.linkedin.com/in/justin-kilburn-3aa38a54/"}} target="_blank"className='externalLink'>LinkedIn</Link>
+                        </div>
+                        {showAboutModal && <AboutModal setShowAboutModal={setShowAboutModal} />}
+                    </div >
+                );
+            };
+        } else {
+            if (sessionUser) {
+                const userId = sessionUser.id
+                return (
+                    <div className="navBar">
+                        <NavLink exact to="/" className="logoNavLink"><img src={logo} alt=""/></NavLink>
+                        <NavLink exact to="/" className="homeNavLink"><p>Home</p></NavLink>
+                        <div className='aboutNav' onClick={() => setShowAboutModal(true)}>About</div>
+                        {showAboutModal && <AboutModal setShowAboutModal={setShowAboutModal} />}
+                        <CreateDropdown />
+                        <SearchBar />
+                        <Link to={{pathname: "https://www.linkedin.com/in/justin-kilburn-3aa38a54/"}} 
+                            target="_blank"
+                            className='navIcon' 
+                            onMouseEnter={() => handleMouseOver('notifications')} 
+                            onMouseOut={() => handleMouseOut('notifications')}>
+                            <i className="fa-brands fa-linkedin"
+                                onMouseEnter={() => handleMouseOver('notifications')} 
+                                onMouseOut={() => handleMouseOut('notifications')}>
+                            </i>
+                        </Link>
+                            {notificationIsHovering && <div className='navTooltip navNotifications'>LinkedIn</div>}
+                        <Link to={{pathname: "https://github.com/Thatmoonman"}}
+                            target="_blank"
+                            className='navIcon'
+                            onMouseEnter={() => handleMouseOver('github')}
+                            onMouseOut={() => handleMouseOut('github')}>
+                            <i className="fa-brands fa-github"
+                                onMouseEnter={() => handleMouseOver('github')}
+                                onMouseOut={() => handleMouseOut('github')}>
+                            </i>
+                        </Link>
+                            {messageIsHovering && <div className='navTooltip navGithub'>Github</div>}
+                        <NavLink exact to={`/users/${userId}`} className='navIcon' 
+                            onMouseEnter={() => handleMouseOver('profile')} 
+                            onMouseOut={() => handleMouseOut('profile')}>
+                            <div className='profileNavPicContainerOuter'>
+                                <div className='profileNavPicContainerInner'>
+                                    <img src={sessionUser.profilePic} alt=""
+                                        onMouseEnter={() => handleMouseOver('profile')} 
+                                        onMouseOut={() => handleMouseOut('profile')}
+                                    />
+                                </div>
+                            </div>
+                        </NavLink>
+                            {profileIsHovering && <div className='navTooltip navProfile' >Your profile</div>}
+                        <ProfileButton user={sessionUser} />
                     </div>
-                    <LoginFormModal />
-                    <SignupFormModal />
-                    {showAboutModal && <AboutModal setShowAboutModal={setShowAboutModal} />}
-                </div>
-            </div >
-        );
-    }
+                );
+            } else {
+                return (
+                    <div className='navBar snap'>
+                        <NavLink exact to="/" className="logoNavLink"><img src="./Hobbypinlogo.png" alt=""/><h1>Hobbypin</h1></NavLink>
+                        <div className='rightNav'>
+                            <div className='externalLinks'>
+                                <div className='externalLink' onClick={() => setShowAboutModal(true)}>About</div>
+                                <Link to={{pathname: "https://github.com/Thatmoonman"}} target="_blank" className='externalLink'>Github</Link>
+                                <Link to={{pathname: "https://www.linkedin.com/in/justin-kilburn-3aa38a54/"}} target="_blank"className='externalLink'>LinkedIn</Link>
+                            </div>
+                            <LoginFormModal />
+                            <SignupFormModal />
+                            {showAboutModal && <AboutModal setShowAboutModal={setShowAboutModal} />}
+                        </div>
+                    </div >
+                );
+            };
+        }
+    };
 
 
     return (
         <div className="navContainer">
-            {sessionLinks}
+            {sessionLinks()}
         </div>
     );
 }
