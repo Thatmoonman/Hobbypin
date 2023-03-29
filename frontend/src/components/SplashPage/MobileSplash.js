e40c5daa78b75f1b8c6cab4253b48969d69f1fc3
Justin Kilburn <ThatMoonMan@gmail.com>
2023-03-29 12:30:00 -0400

    resize

--- frontend/src/components/SplashPage/MobileSplash.js
+++ frontend/src/components/SplashPage/MobileSplash.js
@@ -4,7 +4,11 @@ import SignupFormModal from "../SignupFormModal"
 const MobileSplashPage = () => {
 
     let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`)
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    window.addEventListener('resize', () => {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    })
 
     // <div className='navBarMobile snap'>
     //                 //     <div className='topNav'>
